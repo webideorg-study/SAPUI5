83881493859b3ae3b13a4822416156373f5a639f
qwertasdfg12345 <tangdunke@gmail.com>
2019-01-20 19:43:15 +0800

    Update on 20190120

---
+++ zui5_master_detail_xmlview/WebContent/13/webapp/controller/aggregation_binding.controller.js
@@ -0,0 +1,55 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], 
+    function(Controller){
+        "use strict";
+        
+        // controller name
+        return Controller.extend("bindingtest.controller.aggregation_binding", {
+            
+            //-------------------------------
+            // initialization
+            //-------------------------------
+            onInit: function() {
+                // binding view with model
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.loadData('models/articles.json');
+                this.getView().setModel(oModel);
+                
+                // Table object add items
+                var oTable = this.getView().byId("table");
+                oTable.bindItems({path: '/', factory: this.createCellsFactory});
+            },
+            
+            //------------------------------------------------
+            // Factory function to add cells for table
+            //------------------------------------------------
+            createCellsFactory: function(sId, oContext) {
+            
+                var oColumnListItem = new sap.m.ColumnListItem(sId, {vAlign: "Middle"});
+                
+                // first two cells are not dynamic
+                oColumnListItem.addCell(new sap.m.Text({text: "{articleName}"}));
+                oColumnListItem.addCell(new sap.m.ObjectMarker({type: "{type}", visibility: sap.m.ObjectMarkerVisibility.IconOnly}));      
+                
+                // third cell is dynamic
+                var oHits = oContext.getProperty("hits");
+                if (oHits >= 100) {
+                    oColumnListItem.addCell(
+                        new sap.ui.layout.VerticalLayout({
+                            content: [
+                               new sap.m.Text({text: "{hits}"}),
+                               new sap.m.ObjectStatus({text:"热门", state:"Success"})
+                            ]
+                        })
+                    )
+                } else {
+                    oColumnListItem.addCell(
+                        new sap.m.Text({text: "{hits}"})
+                    )       
+                }
+        
+                return oColumnListItem;
+            } // end of createCellsFactory()            
+            
+        });
+    }
+);
